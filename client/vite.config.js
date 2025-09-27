1ee3e643e905173ee6b44c53ac07cd259e1b43d6
jaustinjr.code <jaustinjr.collab@gmail.com>
2025-09-26 22:12:03 -0700

    Remove Logs on Build
    Reference: https://github.com/vitejs/vite/discussions/7920

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
   plugins: [react()],
   base: "/",
   resolve: {
