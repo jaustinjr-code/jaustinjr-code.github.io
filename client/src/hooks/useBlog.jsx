6203f3d9dc679da02eda2314649a974a23f45a47
jaustinjr.code <jaustinjr.collab@gmail.com>
2026-04-15 10:35:37 -0700

    Integrate medium-parser
    Implements the parseFeed function and installs the local medium-parser package path.

--- client/src/hooks/useBlog.jsx
+++ client/src/hooks/useBlog.jsx
@@ -1,34 +1,40 @@
 import { useEffect, useState } from "react";
-import useRssParser from "./useRssParser";
+// import useRssParser from "./useRssParser";
+import { parseFeed } from "medium-parser/parser";
 
 export default function useBlog() {
   const [articles, setArticles] = useState([]);
-  const { getFeedItems, parseImageFromFeed } = useRssParser();
+  // const { getFeedItems, parseImageFromFeed } = useRssParser();
 
   useEffect(() => {
     const createArticleEmbeds = async () => {
-      const liveArticles = await getFeedItems();
+      // const liveArticles = await getFeedItems();
 
-      if (!(liveArticles && liveArticles.items)) {
-        return;
-      }
+      // if (!(liveArticles && liveArticles.items)) {
+      //   return;
+      // }
 
-      let articleEmbeds = [];
-      liveArticles.items.forEach((item) => {
-        let img =
-          item.content === undefined
-            ? parseImageFromFeed(item["content:encoded"])
-            : parseImageFromFeed(item.content);
+      // let articleEmbeds = [];
+      // liveArticles.items.forEach((item) => {
+      //   let img =
+      //     item.content === undefined
+      //       ? parseImageFromFeed(item["content:encoded"])
+      //       : parseImageFromFeed(item.content);
 
-        articleEmbeds.push({
-          ...item,
-          image: img,
-        });
+      //   articleEmbeds.push({
+      //     ...item,
+      //     image: img,
+      //   });
+      // });
+
+      const articleEmbeds = await parseFeed("@jaustinjr.blog").catch((err) => {
+        console.error("Parse feed failed.", err);
+        return [];
       });
       setArticles(articleEmbeds);
     };
     createArticleEmbeds();
-  }, [getFeedItems, parseImageFromFeed]);
+  }, []);
 
   return { articles };
 }
