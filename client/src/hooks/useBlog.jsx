dae3d632b51218cf2b41981456ba08ce828f2e79
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-05-01 19:52:48 -0700

    Use medium-parser to retrieve article feed

--- client/src/hooks/useBlog.jsx
+++ client/src/hooks/useBlog.jsx
@@ -1,37 +1,16 @@
 import { useEffect, useState } from "react";
-// import useRssParser from "./useRssParser";
 import { parseFeed } from "medium-parser/parser";
 
 export default function useBlog() {
   const [articles, setArticles] = useState([]);
-  // const { getFeedItems, parseImageFromFeed } = useRssParser();
 
   useEffect(() => {
     const createArticleEmbeds = async () => {
-      // const liveArticles = await getFeedItems();
-
-      // if (!(liveArticles && liveArticles.items)) {
-      //   return;
-      // }
-
-      // let articleEmbeds = [];
-      // liveArticles.items.forEach((item) => {
-      //   let img =
-      //     item.content === undefined
-      //       ? parseImageFromFeed(item["content:encoded"])
-      //       : parseImageFromFeed(item.content);
-
-      //   articleEmbeds.push({
-      //     ...item,
-      //     image: img,
-      //   });
-      // });
-
-      const articleEmbeds = await parseFeed("@jaustinjr.blog").catch((err) => {
+      const articleFeed = await parseFeed("@jaustinjr").catch((err) => {
         console.error("Parse feed failed.", err);
         return [];
       });
-      setArticles(articleEmbeds);
+      setArticles(articleFeed.items);
     };
     createArticleEmbeds();
   }, []);
